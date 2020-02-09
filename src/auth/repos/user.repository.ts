14827797cd6eab99b310f7782d86e112a15f6c13
Alex <mr.portmane@gmail.com>
2020-02-09 04:52:46 -0800

    user.repository.ts v - 0.0.1 <<- Comment: "Repository for User entity.", Module: "Auth" ->>

---
+++ src/auth/repos/user.repository.ts
@@ -0,0 +1,47 @@
+import {EntityRepository, Repository} from "typeorm";
+import {User} from "../domain/user.entity";
+import {AuthCredentialsDto} from "../dto/auth-credentials.dto";
+import {ConflictException, InternalServerErrorException} from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const {username, password} = authCredentialsDto;
+
+
+
+        const user = new User();
+        user.username = username;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+
+        try {
+             await user.save();
+        } catch (e) {
+            if (e.code === '23505') { // Duplicate username.
+                throw new ConflictException('Username is already exists.')
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+
+
+    async validationUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const {username, password} = authCredentialsDto;
+        const user = await this.findOne({username});
+
+        if (user && await user.validatePassword(password)) {
+            return user.username;
+        } else {
+            return null;
+        }
+    }
+
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
+}
