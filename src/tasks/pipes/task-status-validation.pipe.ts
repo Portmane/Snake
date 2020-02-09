74a726568dac58292d34eec7f03f8267097a8024
Alex <mr.portmane@gmail.com>
2020-02-09 04:46:46 -0800

    task-status-validation.pipe.ts v - 0.0.1 <<- Comment: "Pipe for verification on the right entered status.", Module: "Tasks" ->>

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,26 @@
+import {BadRequestException, PipeTransform} from "@nestjs/common";
+import {TaskStatus} from "../enums/TaskStatus.enum";
+
+export class TaskStatusValidationPipe implements PipeTransform{
+    readonly allowedStatuses = [
+        TaskStatus.OPEN,
+        TaskStatus.DONE,
+        TaskStatus.IN_PROGRESS,
+    ];
+
+
+
+    transform(value: any): any {
+        value = value.toUpperCase();
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`"${value}" is an invalid status.`)
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any): boolean {
+        const index = this.allowedStatuses.indexOf(status);
+        return index !== -1;
+    }
+}
