a0300c0144c162f71cc602447ddef57a75ee58fd
Alex <mr.portmane@gmail.com>
2020-02-14 11:36:38 -0800

    tasks.module.ts v - 0.0.1 <<- Comment: "Configuration of the Tasks module.", Module: "Tasks" ->>

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,11 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {TaskRepository} from "./repos/TaskRepository.repository";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
   imports: [
+      AuthModule,
       TypeOrmModule.forFeature([TaskRepository]),
   ],
   controllers: [TasksController],
