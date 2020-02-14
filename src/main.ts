0fa38fb7ab66f7e0051819b28fc3c6fb2f2a0716
Alex <mr.portmane@gmail.com>
2020-02-14 11:35:29 -0800

    main.ts v - 0.0.2 <<- Comment: "Starting of the app.", Module: "Main" ->>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Logger} from "@nestjs/common";
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfiguration = config.get('server');
+  const logger = new Logger("Bootstrap logger");
+
+
+
+  const port = process.env.PORT || serverConfiguration.port;      // ASK
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+
+    logger.log(`Application listening on port ${port}`);  // Logs
 }
 bootstrap();
