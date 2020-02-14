d5e87a752d5a49381e9b6c573f478417d1a57974
Alex <mr.portmane@gmail.com>
2020-02-14 11:39:31 -0800

    auth.controller.ts v - 0.0.2 <<- Comment: "Controller for Auth module. Return type of the signing in process.", Module: "Auth" ->>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post, ValidationPipe} from '@nestjs/common';
+import {Body, Controller, Post, Req, UseGuards, ValidationPipe} from '@nestjs/common';
 import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
 import {AuthService} from "./auth.service";
 
@@ -18,7 +18,7 @@ export class AuthController {
 
 
     @Post('/signin')
-    async signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    async signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialsDto);
     }
 }
